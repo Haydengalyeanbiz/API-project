d0266365091620ca7558672b171a7357a2fe3b00
Hayden <haydengalyeanbiz@gmail.com>
2024-07-12 17:34:37 -0400

    finished phase 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 // frontend/src/App.jsx
-// frontend/src/App.jsx
 
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import SignupFormPage from './components/SignupFromPage';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage/index.js';
+import SignupFormPage from './components/SignupFormPage/index.js';
+import Navigation from './components/Navigation/index.js';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -18,7 +18,12 @@ function Layout() {
 		});
 	}, [dispatch]);
 
-	return <>{isLoaded && <Outlet />}</>;
+	return (
+		<>
+			<Navigation isLoaded={isLoaded} />
+			{isLoaded && <Outlet />}
+		</>
+	);
 }
 
 const router = createBrowserRouter([
@@ -30,11 +35,11 @@ const router = createBrowserRouter([
 				element: <h1>Welcome!</h1>,
 			},
 			{
-				path: '/login',
+				path: 'login',
 				element: <LoginFormPage />,
 			},
 			{
-				path: '/signup',
+				path: 'signup',
 				element: <SignupFormPage />,
 			},
 		],
